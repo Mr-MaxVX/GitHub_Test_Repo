c360b89d7f8990782e6e2fa0583d9c6a6be6d129
Mr.Max VX <max502ksa@gmail.com>
2025-01-18 12:17:00 +0300

    The new nested loop conditional isn't good

--- Testing.java
+++ Testing.java
@@ -1,22 +1,55 @@
+/*
+*
+* Auth@r : Bassam
+*
+* I thought the shape will be beautiful so I really learned how to do a nested loop
+*
+* I wish u like it and yah enjoy (*￣▽￣*)ブ
+*
+*
+*
+* */
+
 public class Testing {
 
-    public static void main(String [] max){
+    public static void main(String[] max) {
 
+        int num;
         System.out.println("**************************");
-        System.out.println("Hello world I'm still living");
-        int i;
-        for (i = 0; i < 5; i++){
+        System.out.println("If u like a number please write it down");
+
+        int i, j;
+
+        for (i = 0; i < 5; i++) {
 
 
             System.out.println("I love programming"); // Adding this will make us even better and more efficient
 
 
+        }
 
+
+        for (i = 0; i < 5; i++) {
+            System.out.println("");
+            for (j = i; j < 5; j++) {
+                System.out.printf("*");
+
+
+            }
+
+
+        }
+        for (i = 1; i <= 5; i++) {
+            System.out.println("");
+            for (j = 0; j < i; j++) {
+
+                System.out.print("*");
+
+
+            }
 
 
         }
 
     }
-
-
 }
