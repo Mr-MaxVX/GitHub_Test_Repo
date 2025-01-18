ae33a3668b5a38f68f5279087f887d8f1dea0e41
Mr.Max VX <max502ksa@gmail.com>
2025-01-18 09:52:40 +0300

    This is the first testing in the Testing.java

---
+++ Testing.java
@@ -0,0 +1,13 @@
+public class Testing {
+
+    public static void main(String [] max){
+
+        System.out.println("**************************");
+        System.out.println("Hello world I'm still living");
+
+
+
+    }
+
+
+}
